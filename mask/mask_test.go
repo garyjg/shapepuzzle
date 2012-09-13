43bdc7b8b3c47dfa7f8d6aa41a9e7d2160cd3b66
Gary Granger <granger@ucar.edu>
2012-09-13 10:06:58 -0600

    refactor mask tests

--- mask/mask_test.go
+++ mask/mask_test.go
@@ -7,34 +7,39 @@ import(
 )
 
 
-func TestMaskEqual(t *testing.T) {
-	bits := MaskBits(0xffffffffffffffff)
-	if bits.Translate(0, 0) != bits {
-	    t.Errorf("(0,0) translation should not change mask.")
-	}
+func testTranslate(t *testing.T, start MaskBits, r int, c int, 
+	 			   expect MaskBits, note string) {
 
+	got := start.Translate(r, c)
+	if got != expect {
+	    t.Errorf("(%d,%d) translation got %v, expected %v: %v", 
+				 r, c, got, expect, note)
+	}
+}
+
+
+func TestMaskEqual(t *testing.T) {
+	all := MaskBits(0xffffffffffffffff)
+	testTranslate(t, all, 0, 0, all, "zero translate should be the same")
 }
 
 func TestMaskNegativeShift(t *testing.T) {
-	bits := MaskBits(0xffffffffffffffff)
-	if bits.Translate(-2, -1) != MaskBits(0xfefefefefefe0000) {
-	    t.Errorf("(-2,-1) translation should have two blank rows and a blank column.")
-	}
+	testTranslate(t, MaskBits(0xffffffffffffffff), -2, -1, 
+					 MaskBits(0xfefefefefefe0000), "")
 }
 
 func TestMaskPositiveShift(t *testing.T) {
-	bits := MaskBits(0xf0f0f0f000000000)
-	if bits.Translate(4, 4) != MaskBits(0x000000000f0f0f0f) {
-	    t.Errorf("(4,4) %v translation should move square to lower right, got %v.",
-	        MaskBits(0xf0f0f0f000000000), bits.Translate(4, 4))
-	}
+	testTranslate(t, MaskBits(0xf0f0f0f000000000), 4, 4, 
+					 MaskBits(0x000000000f0f0f0f), "move square to lower right")
 }
 
 func TestMaskShiftTooFar(t *testing.T) {
-	bits := MaskBits(0xf0f0f0f000000000)
-	if bits.Translate(4, 5) != MaskBits(0x0000000007070707) {
-	    t.Errorf("(4,4) %v translation should move square to lower right, got %v.",
-	        MaskBits(0xf0f0f0f000000000), bits.Translate(4, 5))
-	}
+	testTranslate(t, MaskBits(0xf0f0f0f000000000), 4, 5, 
+					 MaskBits(0x0000000007070707), "move past lower right")
 }
 
+func TestZero(t *testing.T) {
+	testTranslate(t, MaskBits(0xf0f0f0f000000000), 8, 8, MaskBits(0),
+					 "move off the board should be zero")
+}
+	
