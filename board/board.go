66e47ea288fc6180b63da813645770bb7446a4d5
Gary Granger <granger@ucar.edu>
2019-02-12 09:22:52 -0700

    fix go lint warnings

--- board/board.go
+++ board/board.go
@@ -3,6 +3,46 @@
 // Package board contains the Board structure and methods for accumulating
 // shape placements on a puzzle grid.
 //
+// Given a set of shapes, a board 8x8, and their permutations and
+// translations on the board, find the combination which covers every spot
+// on the board.  The board state is represented as a 64-bit mask, and
+// shape placements can be converted to a mask on that board to quickly
+// determine if that placement is valid, ie, it does not collide with any
+// other placements.
+//
+// As soon as a board state is found on which a piece cannot be placed,
+// that board state is discarded and not propagated.  Therefore not all
+// pieces will need to be attempted before a placement sequence is
+// recognized as a dead end.
+//
+// Because the first placement is symmetrical across the four quadrants of
+// the board, it only needs to generate placements in one quadrant.  That
+// at least reduces the search space by close to a fourth.
+//
+// This might be optimized by immediately pruning board states which cannot
+// yield solutions because there are gaps which are too small for any shape.
+// These gaps can be found by matching against a set of masks:
+//
+//  A square with one space inside, shifted to all positions on the board.
+//  A space on the edge surrounded by filled positions.
+//  A rectangle with two spaces inside.
+//  A rectangle on the edge.
+//  Corner masks which only fit one, two, three, or four spaces.
+//
+// [ [ 0, 1, 0 ], [ 1, 0, 1 ], [ 0, 1, 0 ] ]
+//
+// The corners do not need to be filled in, only the edges.  The point is
+// that empty spaces are surrounded on all sides by either filled spaces or
+// the border.
+//
+// Basically, given a rectangle with 1, 2, 3, or 4 empty spaces inside, AND
+// that filled shape with the board, then OR the mask with the inside
+// spaces removed.  If the result is not equal to the filled mask, then
+// there are gaps which cannot fit a shape.
+//
+// Also, the masks for a shape placement do not change depending upon the
+// incoming board, so really the placements can all be computed ahead of
+// time and then the masks scanned for one that fits on the incoming board.
 package board
 
 import (
@@ -31,34 +71,45 @@ func NewBoard(nrows int, ncols int) Board {
 	return nb
 }
 
+// NumShapes returns the number of shapes placed on the board.
 func (b Board) NumShapes() int {
 	return len(b.placements)
 }
 
+// NumRows returns number of rows in the board.
 func (b Board) NumRows() int {
 	return b.nrows
 }
 
+// NumCols returns number of columns in the board.
 func (b Board) NumCols() int {
 	return b.ncols
 }
 
+// Mask returns the current mask for the board.
 func (b Board) Mask() mask.Bits {
 	return b.mask
 }
 
+// RegionMask creates a mask which matches all the points on the board.
+// For example, the RegionMask for a 5x5 Board will have the first 5 bits
+// set of the first 5 bytes, corresponding to the upper left 5x5 grid
+// of the full 8x8 bit mask.
 func (b Board) RegionMask() mask.Bits {
 	cbits := mask.Bits(0)
-	for c := 0; c < b.NumCols(); c += 1 {
+	for c := 0; c < b.NumCols(); c++ {
 		cbits = mask.FirstBit() | (cbits >> 1)
 	}
 	mbits := mask.Bits(0)
-	for r := 0; r < b.NumRows(); r += 1 {
+	for r := 0; r < b.NumRows(); r++ {
 		mbits = (mbits >> 8) | cbits
 	}
 	return mbits
 }
 
+// Place adds the Shape to the Board and returns the new Board.  Note the
+// original board is not changed, and there is no check that the Shape fits.
+// This just adds the Shape to list of placements and updates the mask.
 func (b Board) Place(p shape.Shape) Board {
 
 	nb := b
@@ -76,10 +127,10 @@ func (b Board) String() string {
 	ncol := b.NumCols()
 	buf := ""
 	grid := make([][]int, nrow)
-	for r := 0; r < nrow; r += 1 {
+	for r := 0; r < nrow; r++ {
 		grid[r] = make([]int, ncol)
 		buf += "["
-		for c := 0; c < ncol; c += 1 {
+		for c := 0; c < ncol; c++ {
 			var mbits mask.Bits
 			mbits = mask.FirstBit().Translate(r, c)
 			for _, p := range b.placements {
@@ -95,73 +146,33 @@ func (b Board) String() string {
 	return buf
 }
 
-// Given a set of shapes, a board 8x8, and their permutations and
-// translations on the board, find the combination which covers every spot
-// on the board.  The board state is represented as a 64-bit mask, and
-// shape placements can be converted to a mask on that board to quickly
-// determine if that placement is valid, ie, it does not collide with any
-// other placements.
-//
-// As soon as a board state is found on which a piece cannot be placed,
-// that board state is discarded and not propagated.  Therefore not all
-// pieces will need to be attempted before a placement sequence is
-// recognized as a dead end.
-//
-// Because the first placement is symmetrical across the four quadrants of
-// the board, it only needs to generate placements in one quadrant.  That
-// at least reduces the search space by close to a fourth.
+// Channel is a channel for passing Board states.
+type Channel chan Board
 
-// This might be optimized by immediately pruning board states which cannot
-// yield solutions because there are gaps which are too small for any shape.
-// These gaps can be found by matching against a set of masks:
-//
-//  A square with one space inside, shifted to all positions on the board.
-//  A space on the edge surrounded by filled positions.
-//  A rectangle with two spaces inside.
-//  A rectangle on the edge.
-//  Corner masks which only fit one, two, three, or four spaces.
-//
-// [ [ 0, 1, 0 ], [ 1, 0, 1 ], [ 0, 1, 0 ] ]
-//
-// The corners do not need to be filled in, only the edges.  The point is
-// that empty spaces are surrounded on all sides by either filled spaces or
-// the border.
-//
-// Basically, given a rectangle with 1, 2, 3, or 4 empty spaces inside, AND
-// that filled shape with the board, then OR the mask with the inside
-// spaces removed.  If the result is not equal to the filled mask, then
-// there are gaps which cannot fit a shape.
+// FirstPlacements generates every permutation of the shape at every position in
+// the quadrant and push it to the channel, unless it matches one of the reject
+// patterns.
+func FirstPlacements(s shape.Shape, b Board, bc Channel) {
 
-// Also, the masks for a shape placement do not change depending upon the
-// incoming board, so really the placements can all be computed ahead of
-// time and then the masks scanned for one that fits on the incoming board.
-
-type BoardChannel chan Board
-
-func FirstPlacements(s shape.Shape, b Board, bc BoardChannel) {
-
-	// Just generate every permutation of the shape at every position in the
-	// quadrant and push it to the channel, unless it matches one of the reject
-	// patterns.
 	rejects := GapShapes(b)
 	perms := s.Permutations()
 	ngen, nrej := 0, 0
 	for _, p := range perms {
 		height := p.NumRows()
 		width := p.NumCols()
-		for r := 0; r <= b.NumRows()/2 && r <= b.NumRows()-height; r += 1 {
-			for c := 0; c <= b.NumCols()/2 && c <= b.NumCols()-width; c += 1 {
+		for r := 0; r <= b.NumRows()/2 && r <= b.NumRows()-height; r++ {
+			for c := 0; c <= b.NumCols()/2 && c <= b.NumCols()-width; c++ {
 				place := p.Translate(r, c)
 				nb := b.Place(place)
 				if !RejectBoard(nb, rejects) {
 					log.Printf("Generating first placement (S#%d):\n%v",
 						place.ID(), nb)
 					bc <- nb
-					ngen += 1
+					ngen++
 				} else {
 					log.Printf("Rejected first placement (S#%d):\n%v",
 						place.ID(), nb)
-					nrej += 1
+					nrej++
 				}
 			}
 		}
@@ -171,20 +182,24 @@ func FirstPlacements(s shape.Shape, b Board, bc BoardChannel) {
 	close(bc)
 }
 
-func NextPlacements(s shape.Shape, base Board, boards BoardChannel,
-	moves BoardChannel) {
+// NextPlacements generates all possible board masks for placing the given shape
+// on a blank Board base.  Then it tries to place each of those permutations on
+// each Board on the boards channel.  Each board on which the shape can be
+// placed successfully is passed to the moves Channel.
+func NextPlacements(s shape.Shape, base Board, boards Channel,
+	moves Channel) {
 
 	// Generate all possible board masks for placing this shape.
 	placements := make([]shape.Shape, 100)
 	rejects := GapShapes(base)
 	placements = placements[0:0]
 	perms := s.Permutations()
-	for i := 0; i < len(perms); i += 1 {
+	for i := 0; i < len(perms); i++ {
 		s := &(perms[i])
 		width := s.NumCols()
 		height := s.NumRows()
-		for r := 0; r <= base.NumRows()-height; r += 1 {
-			for c := 0; c <= base.NumCols()-width; c += 1 {
+		for r := 0; r <= base.NumRows()-height; r++ {
+			for c := 0; c <= base.NumCols()-width; c++ {
 				place := (*s).Translate(r, c)
 				// If this shape at this place on a blank board would be
 				// rejected, then reject it for any board.
@@ -214,29 +229,28 @@ func NextPlacements(s shape.Shape, base Board, boards BoardChannel,
 	close(moves)
 }
 
-// To search the solution space, each piece gets its own goroutine in which
-// it generates all the available moves on the current board state.  It
-// puts each valid move and the resulting board state onto its output
-// channel.  The goroutines for each piece can be chained together so that
-// if the final piece in the chain puts a new board state on its output
-// channel, then that must be a solution to the puzzle.
+// Solve searches the solution space: each piece gets its own goroutine in which
+// it generates all the available moves on the current board state.  It puts
+// each valid move and the resulting board state onto its output channel.  The
+// goroutines for each piece can be chained together so that if the final piece
+// in the chain puts a new board state on its output channel, then that must be
+// a solution to the puzzle.
 //
-
-func (b Board) Solve(shapes []shape.Shape) BoardChannel {
+func (b Board) Solve(shapes []shape.Shape) Channel {
 
 	nshapes := len(shapes)
 
 	// Set up a channel for each shape to be placed.
-	channels := make([]BoardChannel, nshapes)
-	for i := 0; i < nshapes; i += 1 {
-		channels[i] = make(BoardChannel, 10000)
+	channels := make([]Channel, nshapes)
+	for i := 0; i < nshapes; i++ {
+		channels[i] = make(Channel, 10000)
 	}
 
 	// Chain the channels.  Generate first placements for the first shape,
 	// and tell it to put those new boards on its channel.
 	go FirstPlacements(shapes[0], b, channels[0])
 
-	for i := 1; i < nshapes; i += 1 {
+	for i := 1; i < nshapes; i++ {
 		go NextPlacements(shapes[i], b, channels[i-1], channels[i])
 	}
 
@@ -274,10 +288,9 @@ func RejectBoard(b Board, patterns []shape.Shape) bool {
 	return searchGap(b, patterns) >= 0
 }
 
-// Given a board with a particular size, generate all the masks which if
-// they match a board should cause the board to be rejected as a potential
-// solution.
-
+// GapShapes, given a board with a particular size, generates all the masks
+// which if they match a board should cause the board to be rejected as a
+// potential solution.
 func GapShapes(b Board) []shape.Shape {
 
 	grids := [][][]int{{
@@ -297,12 +310,12 @@ func GapShapes(b Board) []shape.Shape {
 	for id, g := range grids {
 		s := shape.NewShape(id+100, g)
 		perms := s.Permutations()
-		for i := 0; i < len(perms); i += 1 {
+		for i := 0; i < len(perms); i++ {
 			s := &(perms[i])
 			width := s.NumCols()
 			height := s.NumRows()
-			for r := -1; r <= b.NumRows()-height+1; r += 1 {
-				for c := -1; c <= b.NumCols()-width+1; c += 1 {
+			for r := -1; r <= b.NumRows()-height+1; r++ {
+				for c := -1; c <= b.NumCols()-width+1; c++ {
 					shapes = append(shapes, s.Translate(r, c).Clip(region))
 				}
 			}
