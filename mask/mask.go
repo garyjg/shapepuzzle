b84c1ae0068dedad2241c6adfe8d53b15cb4b678
Gary Granger <granger@ucar.edu>
2019-09-11 14:49:55 -0600

    go fmt fixes

--- mask/mask.go
+++ mask/mask.go
@@ -6,13 +6,12 @@
 package mask
 
 import (
-    "fmt"
+	"fmt"
 )
 
 // Bits type is an 8-byte unsigned integer to efficiently represent a
 // 2D mask on any grid up to 8x8.
 type Bits uint64
-
 
 // FirstBit returns a Bits mask initialized with only the very
 // first bit set, corresponding to the upper left corner of a grid.
@@ -20,13 +19,11 @@ func FirstBit() Bits {
 	return Bits(0x8000000000000000)
 }
 
-
 // String method of Bits formats the mask into a hexadecimal
 // representation.
 func (mask Bits) String() string {
 	return fmt.Sprintf("0x%016x", uint64(mask))
 }
-
 
 // ComputeMask turns a 2D array or slice into a mask on a nrow X ncol grid.
 // The most-significant bit in the mask is for the upper left corner of the
@@ -38,20 +35,19 @@ func ComputeMask(grid [][]int) (Bits, Bits) {
 	mbits := Bits(0)
 	gapbits := Bits(0)
 	for r := 0; r < len(grid); r++ {
-		bit := FirstBit() >> uint(r * 8)
+		bit := FirstBit() >> uint(r*8)
 		for c := 0; c < len(grid[0]); c++ {
 			if grid[r][c] != 0 {
 				mbits = mbits | bit
 			}
 			if grid[r][c] == 2 {
-			    gapbits = gapbits | bit
+				gapbits = gapbits | bit
 			}
 			bit = bit >> 1
 		}
 	}
 	return mbits, gapbits
 }
-
 
 // Translate adjusts mask as if the bit pattern on the grid were translated
 // by row and col places.  It is possible to translate a mask past the
@@ -60,28 +56,27 @@ func ComputeMask(grid [][]int) (Bits, Bits) {
 func (mask Bits) Translate(row int, col int) Bits {
 
 	if row < 0 {
-	    mask = mask << uint(-row * 8)
+		mask = mask << uint(-row*8)
 		row = 0
 	} else {
-	    mask = mask >> uint(row * 8)
+		mask = mask >> uint(row*8)
 	}
 
 	// Truncating columns means ANDing each byte before shifting it to the
 	// right column.
-    blank := uint64(0)
+	blank := uint64(0)
 	if col < 0 {
 		for i := 0; i < 8; i++ {
-		    blank = (blank << 8) + (0xff >> uint(-col))
+			blank = (blank << 8) + (0xff >> uint(-col))
 		}
 		mask = mask & Bits(blank)
 		mask = mask << uint(-col)
 	} else {
 		for i := 0; i < 8; i++ {
-		    blank = (blank << 8) + ((0xff << uint(col)) & 0xff)
+			blank = (blank << 8) + ((0xff << uint(col)) & 0xff)
 		}
 		mask = mask & Bits(blank)
 		mask = mask >> uint(col)
-	}   
+	}
 	return mask
 }
-
--- mask/mask_test.go
+++ mask/mask_test.go
@@ -2,21 +2,19 @@
 
 package mask
 
-import(
-    "testing"
+import (
+	"testing"
 )
 
-
-func testTranslate(t *testing.T, start Bits, r int, c int, 
-	 			   expect Bits, note string) {
+func testTranslate(t *testing.T, start Bits, r int, c int,
+	expect Bits, note string) {
 
 	got := start.Translate(r, c)
 	if got != expect {
-	    t.Errorf("(%d,%d) translation got %v, expected %v: %v", 
-				 r, c, got, expect, note)
+		t.Errorf("(%d,%d) translation got %v, expected %v: %v",
+			r, c, got, expect, note)
 	}
 }
-
 
 func TestMaskEqual(t *testing.T) {
 	all := Bits(0xffffffffffffffff)
@@ -24,22 +22,21 @@ func TestMaskEqual(t *testing.T) {
 }
 
 func TestMaskNegativeShift(t *testing.T) {
-	testTranslate(t, Bits(0xffffffffffffffff), -2, -1, 
-					 Bits(0xfefefefefefe0000), "")
+	testTranslate(t, Bits(0xffffffffffffffff), -2, -1,
+		Bits(0xfefefefefefe0000), "")
 }
 
 func TestMaskPositiveShift(t *testing.T) {
-	testTranslate(t, Bits(0xf0f0f0f000000000), 4, 4, 
-					 Bits(0x000000000f0f0f0f), "move square to lower right")
+	testTranslate(t, Bits(0xf0f0f0f000000000), 4, 4,
+		Bits(0x000000000f0f0f0f), "move square to lower right")
 }
 
 func TestMaskShiftTooFar(t *testing.T) {
-	testTranslate(t, Bits(0xf0f0f0f000000000), 4, 5, 
-					 Bits(0x0000000007070707), "move past lower right")
+	testTranslate(t, Bits(0xf0f0f0f000000000), 4, 5,
+		Bits(0x0000000007070707), "move past lower right")
 }
 
 func TestZero(t *testing.T) {
 	testTranslate(t, Bits(0xf0f0f0f000000000), 8, 8, Bits(0),
-					 "move off the board should be zero")
+		"move off the board should be zero")
 }
-	
